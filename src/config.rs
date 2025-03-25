f1d6552efe5dcd13dde37fb147f1459d1677d60d
nixxoq <nixxoq@outlook.com>
2025-03-25 21:24:30 +0200

    config.rs: cleanup code (client line is now unused)

--- src/config.rs
+++ src/config.rs
@@ -1,20 +1,16 @@
 use dotenv::dotenv;
-use gemini_client_rs::GeminiClient;
-use std::sync::Arc;
 use teloxide::prelude::*;
 
 #[derive(Clone)]
 pub struct Config {
     bot: Bot,
     owners: Vec<i64>,
-    client: Arc<GeminiClient>,
 }
 
 impl Config {
     pub async fn new() -> Self {
         dotenv().ok();
 
-        let ai_key = std::env::var("GEMINI_API_KEY").expect("GEMINI_API_KEY expected");
         let bot_token = std::env::var("BOT_TOKEN").expect("BOT_TOKEN expected");
         let bot = Bot::new(bot_token);
 
@@ -24,11 +20,7 @@ impl Config {
             .filter_map(|id| id.trim().parse().ok())
             .collect();
 
-        Config {
-            bot,
-            owners,
-            client: Arc::new(GeminiClient::new(ai_key)),
-        }
+        Config { bot, owners }
     }
 
     pub fn get_bot(&self) -> &Bot {
@@ -37,9 +29,5 @@ impl Config {
 
     pub fn get_owners(&self) -> &Vec<i64> {
         &self.owners
-    }
-
-    pub fn get_client(&self) -> Arc<GeminiClient> {
-        self.client.clone()
     }
 }
