62155bc4847d488f0af3359dc11be346e0f8ab5b
nixxoq <nixxoq@outlook.com>
2025-03-25 21:25:24 +0200

    transcription.rs: finally working voice recognition

--- src/util/transcription.rs
+++ src/util/transcription.rs
@@ -1,32 +1,38 @@
 use crate::config::Config;
 use crate::util::errors::MyError;
-use base64::encode;
 use bytes::Bytes;
-use gemini_client_rs::types::{Candidate, Content, ContentPart, GenerateContentRequest, PartResponse, Role};
-use std::error::Error;
-use std::io::Read;
+use teloxide::Bot;
 use teloxide::payloads::SendMessageSetters;
 use teloxide::requests::{Request as TeloxideRequest, Requester};
 use teloxide::types::{Message, MessageKind, ParseMode};
-use teloxide::Bot;
+
+use super::enums::AudioStruct;
 
 pub async fn transcription_handler(bot: Bot, msg: Message, config: &Config) -> Result<(), MyError> {
-    if let Some(file_id) = get_file_id(&msg) {
-        let file_data = save_file_to_memory(&bot, &file_id).await?;
+    if let Some(file) = get_file_id(&msg) {
+        let file_data = save_file_to_memory(&bot, &file.file_id).await?;
         let transcription = Transcription {
+            mime_type: file.mime_type,
             data: file_data,
             config: config.clone(),
         };
+
+        let message = bot
+            .send_message(msg.chat.id, "Подождите...")
+            .parse_mode(ParseMode::Html)
+            .await
+            .ok();
+
         let text = transcription.to_text().await;
 
-        bot.send_message(
+        bot.edit_message_text(
             msg.chat.id,
+            message.unwrap().id,
             format!(
                 "Транскрипция: {}",
-                text.unwrap_or_else(|e| format!("Ошибка: {}", e))
+                text.first().unwrap_or(&String::from("Нет данных"))
             ),
         )
-        .parse_mode(ParseMode::Html)
         .await?;
     } else {
         bot.send_message(msg.chat.id, "Не удалось найти голосовое сообщение.")
@@ -36,14 +42,33 @@ pub async fn transcription_handler(bot: Bot, msg: Message, config: &Config) -> R
     Ok(())
 }
 
-pub fn get_file_id(msg: &Message) -> Option<String> {
+pub fn get_file_id(msg: &Message) -> Option<AudioStruct> {
     match &msg.kind {
         MessageKind::Common(common) => match &common.media_kind {
-            teloxide::types::MediaKind::Audio(audio) => Some(audio.audio.file.id.clone()),
-            teloxide::types::MediaKind::Voice(voice) => Some(voice.voice.file.id.clone()),
-            teloxide::types::MediaKind::VideoNote(video_note) => {
-                Some(video_note.video_note.file.id.clone())
-            }
+            teloxide::types::MediaKind::Audio(audio) => Some(AudioStruct {
+                mime_type: audio
+                    .audio
+                    .mime_type
+                    .as_ref()
+                    .unwrap()
+                    .essence_str()
+                    .to_owned(),
+                file_id: audio.audio.file.id.to_owned(),
+            }),
+            teloxide::types::MediaKind::Voice(voice) => Some(AudioStruct {
+                mime_type: voice
+                    .voice
+                    .mime_type
+                    .as_ref()
+                    .unwrap()
+                    .essence_str()
+                    .to_owned(),
+                file_id: voice.voice.file.id.to_owned(),
+            }),
+            teloxide::types::MediaKind::VideoNote(video_note) => Some(AudioStruct {
+                mime_type: "video/mp4".to_owned(),
+                file_id: video_note.video_note.file.id.to_owned(),
+            }),
             _ => None,
         },
         _ => None,
@@ -65,45 +90,72 @@ pub async fn save_file_to_memory(bot: &Bot, file_id: &str) -> Result<Bytes, MyEr
 }
 
 pub struct Transcription {
+    pub(crate) mime_type: String,
     pub(crate) data: Bytes,
     pub(crate) config: Config,
 }
 
 impl Transcription {
-    pub async fn to_text(self) -> Result<String, Box<dyn Error>> {
-        let request = GenerateContentRequest {
-            contents: vec![Content {
-                parts: vec![ContentPart::Text(
-                    "Сделай транскрипцию голосового сообщения:\n".to_owned() + self.data.,
-                )],
-                role: Role::User,
-            }],
-            tools: None,
-        };
+    pub async fn to_text(self) -> Vec<String> {
+        // self.data
+        // let content: Vec<u8> = fs::read("audio.ogg").unwrap();
+        let settings = gem_rs::types::Settings::new();
 
-        let response = self
-            .config
-            .get_client()
-            .generate_content("gemini-2.0-flash-exp", &request)
-            .await?;
+        let mut client = gem_rs::client::GemSession::Builder()
+            .model(gem_rs::api::Models::Custom(
+                "gemini-2.0-flash-thinking-exp".to_owned(),
+            ))
+            .build();
+        // let ap = gem_rs::client::GemSession::from(self.config.get_client())
+        let response = client
+            .send_message_with_blob(
+                "Ретранслируй голосовое сообщение, при этом соблюдая оригинальный язык голосовухи",
+                gem_rs::types::Blob::new(&self.mime_type, &self.data),
+                gem_rs::types::Role::User,
+                &settings,
+            )
+            .await
+            .unwrap();
 
-        let response_string = Self::candidate_to_string(response.candidates);
+        response.get_results()
+        // results[0];
 
-        println!("{}", response_string);
-        Ok(response_string)
+        // println!("{:#?}", results)
     }
+    // pub async fn to_text(self) -> Result<String, Box<dyn Error>> {
+    //     let request = GenerateContentRequest {
+    //         contents: vec![Content {
+    //             parts: vec![ContentPart::Text(
+    //                 "Сделай транскрипцию голосового сообщения:\n".to_owned() + self.data.,
+    //             )],
+    //             role: Role::User,
+    //         }],
+    //         tools: None,
+    //     };
 
-    fn candidate_to_string(candidates: Option<Vec<Candidate>>) -> String {
-        let mut response = String::new();
-        if let Some(candidates) = candidates {
-            for candidate in candidates {
-                for part in candidate.content.parts {
-                    if let PartResponse::Text(text) = part {
-                        response = text.clone();
-                    }
-                }
-            }
-        }
-        response
-    }
+    //     let response = self
+    //         .config
+    //         .get_client()
+    //         .generate_content("gemini-2.0-flash-exp", &request)
+    //         .await?;
+
+    //     let response_string = Self::candidate_to_string(response.candidates);
+
+    //     println!("{}", response_string);
+    //     Ok(response_string)
+    // }
+
+    // fn candidate_to_string(candidates: Option<Vec<Candidate>>) -> String {
+    //     let mut response = String::new();
+    //     if let Some(candidates) = candidates {
+    //         for candidate in candidates {
+    //             for part in candidate.content.parts {
+    //                 if let PartResponse::Text(text) = part {
+    //                     response = text.clone();
+    //                 }
+    //             }
+    //         }
+    //     }
+    //     response
+    // }
 }
