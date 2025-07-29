a42f0fa00e9bc803b1c757edf62667d7b22b6361
nixxoq <nixxoq@outlook.com>
2025-07-29 09:57:02 +0300

    feat(voice): check if chat member is original author or administrator to delete transcript message

--- src/handlers/markups/inlines/delete_msg_inline.rs
+++ src/handlers/markups/inlines/delete_msg_inline.rs
@@ -1,28 +1,33 @@
 use crate::util::errors::MyError;
+use crate::util::inline::DELETE_CALLBACK_DATA;
+use teloxide::Bot;
 use teloxide::payloads::AnswerCallbackQuerySetters;
 use teloxide::prelude::CallbackQuery;
 use teloxide::requests::Requester;
-use teloxide::Bot;
 
-pub async fn delete_msg_handler(
-    bot: Bot,
-    q: CallbackQuery,
-) -> Result<(), MyError> {
+pub async fn delete_msg_handler(bot: Bot, q: CallbackQuery) -> Result<(), MyError> {
     if let Some(data) = q.data {
         let parts: Vec<&str> = data.split(':').collect();
-        if parts.len() == 2 && parts[0] == crate::util::inline::DELETE_CALLBACK_DATA {
+
+        if parts.len() == 2 && parts[0] == DELETE_CALLBACK_DATA {
             if let Ok(original_user_id) = parts[1].parse::<u64>() {
-                if q.from.id.0.eq(&original_user_id) {
-                    if let Some(message) = q.message {
-                        bot.delete_message(message.chat().id, message.id()).await?;
+                if let Some(message) = q.message {
+                    let chat_id = message.chat().id;
+                    let member = bot.get_chat_member(chat_id, q.from.id).await;
+
+                    if let Ok(member) = member {
+                        if member.user.id.0.eq(&original_user_id) || member.is_privileged() {
+                            bot.delete_message(chat_id, message.id()).await?;
+                            return Ok(());
+                        }
                     }
-                } else {
-                    bot.answer_callback_query(q.id)
-                        .text("❌ Вы не можете удалить чужое сообщение")
-                        .show_alert(true)
-                        .await?;
-                    return Ok(());
                 }
+
+                bot.answer_callback_query(q.id)
+                    .text("❌ Произошла неопознанная ошибка")
+                    .show_alert(true)
+                    .await?;
+                return Ok(());
             }
         }
     }
