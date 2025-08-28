0b7fa38c4e55b58f4898184778e43a7cb8a5f6ad
nixxoq <nixxoq@outlook.com>
2025-08-28 13:15:49 +0300

    feat(loader): reorganize dependency tree, always drop pending updates by default

--- src/loader.rs
+++ src/loader.rs
@@ -1,12 +1,20 @@
+use crate::handlers::markups::inlines::currency::handle_currency_inline;
+use crate::handlers::messages::messager::{handle_currency, handle_speech};
+use crate::util::currency::converter::is_currency_query;
 use crate::{
     config::Config,
     handlers::{
         commander::command_handlers, markups::markuper::callback_query_handlers,
-        messages::messager::messages_handlers,
+        messages::chat::handle_bot_added,
     },
     util::{enums::Command, errors::MyError},
 };
+use log::info;
 use oximod::set_global_client;
+use std::sync::Arc;
+use teloxide::dispatching::MessageFilterExt;
+use teloxide::prelude::{Handler, LoggingErrorHandler, Message};
+use teloxide::update_listeners::Polling;
 use teloxide::{
     dispatching::{Dispatcher, HandlerExt, UpdateFilterExt},
     dptree,
@@ -15,49 +23,63 @@ use teloxide::{
     utils::command::BotCommands,
 };
 
-async fn run_bot(config: &Config) -> Result<(), MyError> {
+pub fn inline_query_handler() -> Handler<
+    'static,
+    Result<(), Box<dyn std::error::Error + Send + Sync>>,
+    teloxide::dispatching::DpHandlerDescription,
+> {
+    dptree::entry().branch(dptree::filter_async(is_currency_query).endpoint(handle_currency_inline))
+}
+
+async fn run_bot(config: Arc<Config>) -> Result<(), MyError> {
     let command_menu = Command::bot_commands();
     config
         .get_bot()
         .set_my_commands(command_menu.clone())
         .await?;
 
-    let commands = Update::filter_message()
-        .filter_command::<Command>()
-        .endpoint(command_handlers);
-
-    let messages = Update::filter_message().endpoint(messages_handlers);
-    let callback_queries = Update::filter_callback_query().endpoint(callback_query_handlers);
-
     let handlers = dptree::entry()
-        .branch(commands)
-        .branch(callback_queries)
-        .branch(messages);
+        .branch(
+            Update::filter_message()
+                .filter_command::<Command>()
+                .endpoint(command_handlers),
+        )
+        .branch(
+            Update::filter_message()
+                .branch(Message::filter_text().endpoint(handle_currency))
+                .branch(Message::filter_voice().endpoint(handle_speech)),
+        )
+        .branch(Update::filter_callback_query().endpoint(callback_query_handlers))
+        .branch(Update::filter_my_chat_member().endpoint(handle_bot_added))
+        .branch(Update::filter_inline_query().branch(inline_query_handler()));
 
+    let bot = config.get_bot().clone();
     let bot_name = config.get_bot().get_my_name().await?;
 
-    println!("Bot name: {:?}", bot_name.name);
+    info!("Bot name: {:?}", bot_name.name);
+    let listener = Polling::builder(bot.clone()).drop_pending_updates().build();
 
-    Dispatcher::builder(config.get_bot().clone(), handlers)
+    Dispatcher::builder(bot.clone(), handlers)
+        .dependencies(dptree::deps![config.clone()])
         .enable_ctrlc_handler()
         .build()
-        .dispatch()
+        .dispatch_with_listener(listener, LoggingErrorHandler::new())
         .await;
     Ok(())
 }
 
-async fn run_database(config: &Config) -> Result<(), MyError> {
+async fn run_database(config: Arc<Config>) -> Result<(), MyError> {
     let url = config.get_mongodb_url().to_owned();
     set_global_client(url.clone()).await?;
 
-    println!("Database connected successfully\nURL: {}", url);
+    info!("Database connected successfully. URL: {}", url);
 
     Ok(())
 }
 
 pub async fn run() -> Result<(), MyError> {
-    let config = Config::new().await;
+    let config = Arc::new(Config::new().await);
 
-    let _th = tokio::join!(run_database(&config), run_bot(&config));
+    let _th = tokio::join!(run_database(config.clone()), run_bot(config.clone()));
     Ok(())
 }
