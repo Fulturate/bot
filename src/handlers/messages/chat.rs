16aeaf32898035af7fd9c6bc7575caec0604e80d
nixxoq <nixxoq@outlook.com>
2025-08-28 14:35:10 +0300

    feat(char): remove from database if bot left/banned from group or user blocked him

--- src/handlers/messages/chat.rs
+++ src/handlers/messages/chat.rs
@@ -1,9 +1,11 @@
 use crate::db::schemas::group::Group;
+use crate::db::schemas::user::User;
 use crate::util::currency::converter::{
     CURRENCY_CONFIG_PATH, CurrencyStruct, get_all_currency_codes,
 };
 use crate::util::errors::MyError;
 use log::{error, info};
+use mongodb::bson::doc;
 use oximod::ModelTrait;
 use teloxide::Bot;
 use teloxide::payloads::SendMessageSetters;
@@ -13,7 +15,19 @@ use teloxide::types::{ChatMemberUpdated, ParseMode};
 pub async fn handle_bot_added(bot: Bot, update: ChatMemberUpdated) -> Result<(), MyError> {
     let id = update.chat.id.to_string();
 
-    if !update.chat.is_chat() || !update.chat.is_group() || !update.chat.is_supergroup() {
+    if update.new_chat_member.is_banned() || update.new_chat_member.is_left() {
+        info!("Administrator is banned or user is blocked me. Deleting from DB");
+
+        let delete_result = if update.chat.is_private() {
+            User::delete(doc! { "user_id": id.clone() }).await
+        } else {
+            Group::delete(doc! { "group_id": id.clone() }).await
+        };
+
+        if let Err(e) = delete_result {
+            error!("Could not delete entity. ID: {} | Error: {}", &id, e);
+        }
+
         return Ok(());
     }
 
@@ -29,20 +43,31 @@ pub async fn handle_bot_added(bot: Bot, update: ChatMemberUpdated) -> Result<(),
         .cloned()
         .collect::<Vec<CurrencyStruct>>();
 
-    let new_group = Group::new()
-        .group_id(id.clone())
-        .convertable_currencies(necessary_codes)
-        .save()
-        .await;
+    let new_query = if update.chat.is_private() {
+        User::new()
+            .user_id(id.clone())
+            .convertable_currencies(necessary_codes)
+            .save()
+            .await
+    } else {
+        Group::new()
+            .group_id(id.clone())
+            .convertable_currencies(necessary_codes)
+            .save()
+            .await
+    };
 
-    if let Err(e) = new_group {
-        error!("Could not save new group. Group id: {} | Error: {}", &id, e);
+    match new_query {
+        Ok(_) => {
+            // todo: welcome message
+            bot.send_message(update.chat.id, "Hello world".to_string())
+                .parse_mode(ParseMode::Html)
+                .await?;
+        }
+        Err(e) => {
+            error!("Could not save new entity. ID: {} | Error: {}", &id, e);
+        }
     }
-
-    // todo: welcome message
-    bot.send_message(update.chat.id, "Hello world".to_string())
-        .parse_mode(ParseMode::Html)
-        .await?;
 
     Ok(())
 }
