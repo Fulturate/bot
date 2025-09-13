fe3b8bc99e2dcf264ec27d0f740e3431b3138f5f
Weever1337 <weever1337@gmail.com>
2025-09-13 13:00:20 +0300

    fixed speech recognition command with "unknown" answer

--- src/bot/commands/speech_recognition.rs
+++ src/bot/commands/speech_recognition.rs
@@ -1,7 +1,9 @@
+use teloxide::dispatching::dialogue::GetChatId;
 use crate::core::config::Config;
 use crate::errors::MyError;
 use crate::core::services::transcription::transcription_handler;
 use teloxide::prelude::*;
+use teloxide::types::ReplyParameters;
 
 pub async fn speech_recognition_handler(
     bot: Bot,
@@ -10,6 +12,10 @@ pub async fn speech_recognition_handler(
 ) -> Result<(), MyError> {
     if msg.reply_to_message().is_some() {
         transcription_handler(bot, msg.reply_to_message().unwrap().clone(), config).await?;
+    } else {
+        bot.send_message(msg.chat_id().unwrap(), "Ответьте на голосовое сообщение.")
+            .reply_parameters(ReplyParameters::new(msg.id))
+            .await?;
     }
     Ok(())
 }
